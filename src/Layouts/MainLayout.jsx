89fc86c4e260dad4e6010b9af4b9cade8d87099f
unknown <asadalgalib9@gmail.com>
2025-01-14 19:58:21 +0600

    project recived and set up complited

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-red-600">This is main</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
